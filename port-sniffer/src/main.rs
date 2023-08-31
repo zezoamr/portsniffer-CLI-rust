c22784624b91f4d1484abcf7618ba24762b760b5
ziad amr shalaby <zezoamr@yandex.com>
2023-08-31 23:37:43 +0300

    refactoring the parsing code + gitignore

--- port-sniffer/src/main.rs
+++ port-sniffer/src/main.rs
@@ -22,13 +22,26 @@ struct CliArgs {
 // accepts a ip v4 or v6 address, must be specified at the end
 
 impl CliArgs {
+
     fn new(args: &Vec<String>) -> Result<CliArgs, &'static str> {
-        
+            match CliArgs::args_check(args) {
+                Ok(args) => return CliArgs::args_parse(args),
+                Err(e) => return Err(e),
+            };
+    }
+
+    fn args_check(args: &Vec<String>) -> Result<&Vec<String>, &'static str> {
         if args.len() < 2 {
             return Err("Invalid arguments, too little arguments");
         } else if args.len() > 4 {
             return Err("Invalid arguments, too many arguments");
         }
+        
+        Ok(args)
+    }
+
+    fn args_parse(args: &Vec<String>) -> Result<CliArgs, &'static str> {
+        
         for i in args {
             if i == "-h" || i == "--help" {
                 return Ok(CliArgs { flag: Flag::Help, thread_count: 0, ip_addr: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)) });
@@ -56,7 +69,7 @@ impl CliArgs {
                 thread_count: 4, // default number of threads is 4
             });
         }
-        return Err("Invalid arguments");
+        Err("Invalid arguments")
     }
 
 }
